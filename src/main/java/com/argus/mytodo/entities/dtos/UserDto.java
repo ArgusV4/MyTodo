a3db6f7f97764ec349086256e2a4523e1986f227
MohamedBSF <oargon.management@gmail.com>
2024-08-13 07:20:55 +0200

    add Password Validator

--- src/main/java/com/argus/mytodo/entities/dtos/UserDto.java
+++ src/main/java/com/argus/mytodo/entities/dtos/UserDto.java
@@ -3,7 +3,10 @@ package com.argus.mytodo.entities.dtos;
 
 import com.argus.mytodo.Enums.RoleENUM;
 import com.argus.mytodo.entities.BaseEntity;
+import com.argus.mytodo.entities.dtos.annotations.ValidPassword;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Data;
 
 import java.util.UUID;
@@ -15,8 +18,10 @@ public class UserDto extends BaseEntity {
     private String firstname;
     private String lastname;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ValidPassword
     private String password;
     private String email;
     private String picture;
+    @Enumerated(EnumType.STRING)
     private RoleENUM role;
 }
